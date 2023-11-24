c1e34badf7636c69ca70c8ab9e5e0704009875cc
Bandzdothatshit <148379554+bandzdothatshit@users.noreply.github.com>
2023-11-24 21:56:12 +0100

    Files upload

---
+++ cl_weaponNames.lua
@@ -0,0 +1,21 @@
+-- Handguns
+
+--WEAPON_G43X
+--WEAPON_BP
+--WEAPON_B70
+--WEAPON_19M
+--WEAPON_G17S
+--WEAPON_HL
+--WEAPON_SHRP
+
+
+
+
+-- Template
+
+--['WEAPON_BP'] = {
+--    label = 'Beretta Pico', -- Name on the images.
+--    weight = 650, -- How much weight will it use?
+--    durability = 0.01,  -- How much will the durability
+--    ammoname = 'ammo-9', -- Ammo the gun will use.
+--},
